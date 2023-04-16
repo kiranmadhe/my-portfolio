d819c4e70524134df8c63e0a74daeff451cc8047
Kiran <kiranmadhe2000@gmail.com>
2023-04-16 21:38:53 +0530

    day 1-1

---
+++ src/components/About.js
@@ -0,0 +1,27 @@
+import React, { useContext, useEffect }  from 'react'
+// import NoteContext from '../context/NoteContext'
+
+// const About = () => {
+//   const a = useContext(NoteContext)
+
+//   useEffect(() => {
+//     a.update()
+//   }, [])
+
+
+//   return (
+//     <>
+//     <p>This is about page, Hi I am  {a.state.name}, My Job role is {a.state.role}</p>
+//     </>
+//   )
+// }
+
+const About = () => {
+    return (
+      <>
+      <p>This is about page</p>
+      </>
+    )
+  }
+
+export default About
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Photo from './photos/photo.png'
+import {
+    Link
+  } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <>
+    
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar bg-body-tertiary">
+        {/* <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={Photo} alt="Logo" width={30} height={24} className="d-inline-block align-text-top" />
+          </a>
+        </div> */}
+      </nav>
+        <div className="container-fluid">
+          {/* <Link className="navbar-brand" to="/home">Kiran Madhe</Link> */}
+          <Link className="navbar-brand" href="#">
+            <img src={Photo} alt="Logo" width={30} height={30} className="d-inline-block align-text-top" />
+              &nbsp;&nbsp;&nbsp;Kiran Madhe
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+              
+              
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
